ba0f926bc68b5d4cbc3f054149214e5c60b183ae
Dpbm <dpbm136@gmail.com>
2024-03-16 15:07:59 -0300

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:nubank_clone/home.dart';
 
-void main(){
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner:false,
-    theme:ThemeData(
-      colorScheme: ColorScheme.fromSwatch().copyWith(secondary: const Color(0xFF280AD1)),
-      splashColor: const Color(0xFF280AD1),
-      highlightColor: const Color(0xFF280AD1),
-      primaryColor: const Color(0xFF280AD1),
-    ),
-    color: const Color(0xFF280AD1),
-    title: 'Nubank Clone',
-    home: const HomeScreenWrapper(),
-  ));
+void main() {
+  runApp(const NubankClone());
+}
+
+class NubankClone extends StatelessWidget {
+  const NubankClone({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return (MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch()
+            .copyWith(secondary: const Color(0xFF280AD1)),
+        splashColor: const Color(0xFF280AD1),
+        highlightColor: const Color(0xFF280AD1),
+        primaryColor: const Color(0xFF280AD1),
+      ),
+      color: const Color(0xFF280AD1),
+      title: 'Nubank Clone',
+      home: const HomeScreenWrapper(),
+    ));
+  }
 }
